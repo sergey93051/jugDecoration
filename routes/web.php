b6110610dbbdaff255701337542f46ecf84f64c9
Sergey <sergey93051@mail.ru>
2020-08-01 20:39:04 +0400

    add wab

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', "AuthUser\MainPageShow@mainshow");
+Route::get('mess/{locale}','AuthUser\MainPageShow@local_lang');
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get("/productinfo/{id}","AuthUser\ProdinfoController@sentProdinfo");
+  
+
+Route::any("/register","AuthUser\AuthRegController@register")->name("register");
+Route::any("/profile","AuthUser\AuthLogController@authorization")->name("profile");
+Route::get("/profile","AuthUser\AuthLogController@showprof");
+Route::post("/logouts","AuthUser\AuthLogController@logouts")->name('logouts');
+
+Route::group(["prefix"=>"/privateurl","middleware"=>['newauth']],function () {
+Route::get("/admin","Admin\AdminController@adminshow")->name("admin");
+Route::get("/showProd","Admin\AddProdController@showadd")->name("showprod");
+Route::any("/addProd","Admin\AddProdController@addProd")->name("addProd");
+
 });
+
+
+
+
